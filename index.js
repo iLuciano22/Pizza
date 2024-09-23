95aa5b7b89b208a8454d09bbe763a3644a24bfa4
iLuciano22 <lucianofromero1@gmail.com>
2024-09-23 14:35:38 -0300

    imagenes y renderizado

--- index.js
+++ index.js
@@ -1,47 +1,84 @@
-const pizzas = [
-  {
-    id: 1,
-    nombre: "pizza de Muzzarella",
-    precio: 500,
-    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
-    imagen: "./img/muzzarella.png",
-  },
-
-  {
-    id: 2,
-    nombre: "pizza de Cebolla",
-    precio: 1500,
-    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
-    imagen: "./img/cebolla.png",
-  },
-
-  {
-    id: 3,
-    nombre: "pizza 4 Quesos",
-    precio: 1380,
-    ingredientes: [
-      "Muzzarella",
-      "Tomate",
-      "Queso Azul",
-      "Parmesano",
-      "Roquefort",
-    ],
-    imagen: "./img/4quesos.png",
-  },
-
-  {
-    id: 4,
-    nombre: "pizza Especial",
-    precio: 1000,
-    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
-    imagen: "./img/especial.png",
-  },
-
-  {
-    id: 5,
-    nombre: "pizza con Anana",
-    precio: 600,
-    ingredientes: ["Muzzarella", "Tomate", "Anana"],
-    imagen: "./img/anana.png",
-  },
+const pizzas = [ 
+  { id: 1, name: "4 Quesos", precio: 800, imagen:"./img/4quesos.png" }, 
+  { id: 2, name: "Cebollada", precio: 800, imagen: "./img/cebolla.png" },
+  { id: 3, name: "Anana", precio: 750, imagen: "./img/anana.png" },
+  { id: 4, name: "Especial", precio: 700, imagen: "./img/especial.png" },
+  { id: 5, name: "Muzza", precio: 650, imagen: "./img/muzzarella.png" },
+  { id: 6, name: "Napolitana", precio: 700, imagen: "./img/napolitana.jpeg" }
 ];
+
+const taskForm = document.querySelector ('.add-form');
+const createCard = document.querySelector ('#container-pizzas')
+
+
+
+
+
+const createProductCard = (pizza) => {
+  const { imagen, precio, name } = pizza;
+  return `
+      <div class="product-card">
+          <h2>${name}</h2>
+          <img src="${imagen}" width="150"/>
+          <p>Precio: $${precio}</p>
+      </div>
+  `
+};
+
+
+const renderProductCard = (pizza) => {
+  createCard.innerHTML = createProductCard(pizza);
+};
+
+
+// Funcion submit del product 
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+
+
+  //
+  const pizzaId = parseInt(document.getElementById ('pizza-id').value);
+
+  // Buscar pizza 
+  const pizzaEncontrada = pizzas.find((pizza) => pizza.id === pizzaId);
+
+
+  // Se renderiza
+
+  if (pizzaEncontrada) {
+      renderProductCard(pizzaEncontrada);
+
+  // localStorage 
+  localStorage.setItem('ultimaPizza', JSON.stringify(pizzaEncontrada));
+
+
+  } else {
+      createCard.innerHTML = `<h3>No se encontro ninguna pizza con ese ID</h3>`;
+  }
+
+
+
+  // Limpiador de busquedas 
+  
+  document.getElementById ('pizza-id').value = '';
+}
+
+
+
+
+// Ultima pizza 
+  const renderUltPizza = () => {
+      const ultimaPizza = localStorage.getItem('ultimaPizza');
+      if (ultimaPizza) {
+          const pizza = JSON.parse(ultimaPizza);
+          renderProductCard(pizza);
+      }
+  }
+
+// funcion iniciadora
+const init = () => {
+  taskForm.addEventListener('submit', handleFormSubmit);
+}
+
+
+init ()
