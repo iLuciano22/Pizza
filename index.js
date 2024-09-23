347801b9c166e2c7c6bb39fd3e1558d30bf83eee
iLuciano22 <lucianofromero1@gmail.com>
2024-09-23 16:08:54 -0300

    precios

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const pizzas = [ 
-  { id: 1, name: "4 Quesos", precio: 800, imagen:"./img/4quesos.png" }, 
-  { id: 2, name: "Cebollada", precio: 800, imagen: "./img/cebolla.png" },
+  { id: 1, name: "4 Quesos", precio: 1400, imagen:"./img/4quesos.png" }, 
+  { id: 2, name: "Cebollada", precio: 1200, imagen: "./img/cebolla.png" },
   { id: 3, name: "Anana", precio: 750, imagen: "./img/anana.png" },
-  { id: 4, name: "Especial", precio: 700, imagen: "./img/especial.png" },
-  { id: 5, name: "Muzza", precio: 650, imagen: "./img/muzzarella.png" },
-  { id: 6, name: "Napolitana", precio: 700, imagen: "./img/napolitana.jpeg" }
+  { id: 4, name: "Especial", precio: 800, imagen: "./img/especial.png" },
+  { id: 5, name: "Muzza", precio: 1000, imagen: "./img/muzzarella.png" },
+  { id: 6, name: "Napolitana", precio: 1500, imagen: "./img/napolitana.jpeg" }
 ];
 
 const taskForm = document.querySelector ('.add-form');
@@ -53,7 +53,7 @@ const handleFormSubmit = (e) => {
 
 
   } else {
-      createCard.innerHTML = `<h3>No se encontro ninguna pizza con ese ID</h3>`;
+      createCard.innerHTML = `<h3>No se encontro ninguna pizza con ese numero</h3>`;
   }
 
 
